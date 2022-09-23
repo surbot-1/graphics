c68fb29a9c6513e5b65fbd103f757958c84845c9
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-09-23 20:26:21 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,18 @@
 function saveAsHex() {} 
-function saveAsText() {} 
+
+function saveAsText() { 
+  var filename = document.getElementById('fname').value;
+  var text = textData;
+  var c = document.createElement('a');
+  c.download = filename + '.txt';
+
+  var t = new Blob([text], {type: 'text/plain'});
+  c.href = window.URL.createObjectURL(t);
+  c.click(); 
+} 
+
 function saveAsRaw() {}
+
 function saveAsImage() {}
+
 function saveAsBase64() {}
